db24ac4e148f0098fb49293cdb4468650653cce0
David Young-Chan Kay <davidykay@gmail.com>
2011-07-23 17:28:29 -0400

    Moved cube below pongsurfaceview.

--- src/com/kongentertainment/pong/PongActivity.java
+++ src/com/kongentertainment/pong/PongActivity.java
@@ -42,7 +42,6 @@ public class PongActivity extends Activity {
   }
 
   private GLSurfaceView mGLSurfaceView;
-
 }
 
 /**
@@ -109,6 +108,142 @@ class GameThread extends Thread {
   public void setRunning(boolean running) {
     mRun = running;
   }
+}
+
+/**
+ * A surface and a renderer for a game of pong.
+ */
+class PongSurfaceView extends GLSurfaceView implements GameSurfaceView {
+
+  private static final String TAG = "PongSurfaceView";
+
+  private Thread mThread;
+
+  public PongSurfaceView(Context context) {
+    super(context);
+    mRenderer = new PongRenderer();
+    setRenderer(mRenderer);
+    setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+    mThread = new GameThread(this);
+    mThread.start();
+  }
+
+  public void updateGameState() {
+    Log.v(TAG, "onTick");
+    // mRenderer.mAngleX += 1;
+    requestRender();
+  }
+
+  @Override public boolean onTrackballEvent(MotionEvent e) {
+    //mRenderer.mAngleX += e.getX() * TRACKBALL_SCALE_FACTOR;
+    //mRenderer.mAngleY += e.getY() * TRACKBALL_SCALE_FACTOR;
+
+    requestRender();
+    return true;
+  }
+
+  @Override public boolean onTouchEvent(MotionEvent e) {
+    float x = e.getX();
+    float y = e.getY();
+    //switch (e.getAction()) {
+    //case MotionEvent.ACTION_MOVE:
+    //  float dx = x - mPreviousX;
+    //  float dy = y - mPreviousY;
+    //  mRenderer.mAngleX += dx * TOUCH_SCALE_FACTOR;
+    //  mRenderer.mAngleY += dy * TOUCH_SCALE_FACTOR;
+    //  requestRender();
+    //}
+    //mPreviousX = x;
+    //mPreviousY = y;
+    return true;
+  }
+
+  private class PongRenderer implements GLSurfaceView.Renderer {
+    public PongRenderer() {
+      mPaddles[0] = new Paddle(Player.HUMAN, new Point(10, 210));
+      mPaddles[1] = new Paddle(Player.COMPUTER, new Point(10, 10));
+    }
+
+    public void onDrawFrame(GL10 gl) {
+      /*
+       * Usually, the first thing one might want to do is to clear
+       * the screen. The most efficient way of doing this is to use
+       * glClear().
+       */
+
+      gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+      /*
+       * Now we're ready to draw some 3D objects
+       */
+
+      gl.glMatrixMode(GL10.GL_MODELVIEW);
+      for (Paddle paddle : mPaddles) {
+        gl.glLoadIdentity();
+
+        // Position the paddle based on its current location.
+        Point location = paddle.getLocation();
+
+        if (paddle.getPlayer() == Player.COMPUTER) {
+          gl.glTranslatef(0.0f, 1.0f, -3.0f);
+        } else {
+          gl.glTranslatef(0.0f, -1.0f, -3.0f);
+        }
+        //gl.glTranslatef(location.x, location.y, -3.0f);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+        paddle.draw(gl);
+      }
+    }
+
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+      gl.glViewport(0, 0, width, height);
+
+      /*
+       * Set our projection matrix. This doesn't have to be done
+       * each time we draw, but usually a new projection needs to
+       * be set when the viewport is resized.
+       */
+
+      float ratio = (float) width / height;
+      gl.glMatrixMode(GL10.GL_PROJECTION);
+      gl.glLoadIdentity();
+      gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+    }
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+      /*
+       * By default, OpenGL enables features that improve quality
+       * but reduce performance. One might want to tweak that
+       * especially on software renderer.
+       */
+      gl.glDisable(GL10.GL_DITHER);
+
+      /*
+       * Some one-time OpenGL initialization can be made here
+       * probably based on features of this particular context
+       */
+      gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                GL10.GL_FASTEST);
+
+
+      gl.glClearColor(1,1,1,1);
+      gl.glEnable(GL10.GL_CULL_FACE);
+      gl.glShadeModel(GL10.GL_SMOOTH);
+      gl.glEnable(GL10.GL_DEPTH_TEST);
+    }
+
+    private Paddle[] mPaddles = new Paddle[2];
+  }
+
+
+  private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+  private final float TRACKBALL_SCALE_FACTOR = 36.0f;
+
+  private PongRenderer mRenderer;
 }
 
 /**
@@ -243,141 +378,4 @@ class CubeSurfaceView extends GLSurfaceView implements GameSurfaceView {
 
   private float mPreviousX;
   private float mPreviousY;
-}
-
-/**
- * Implement a simple rotation control.
- *
- */
-class PongSurfaceView extends GLSurfaceView implements GameSurfaceView {
-
-  private static final String TAG = "PongSurfaceView";
-
-  private Thread mThread;
-
-  public PongSurfaceView(Context context) {
-    super(context);
-    mRenderer = new PongRenderer();
-    setRenderer(mRenderer);
-    setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-
-    mThread = new GameThread(this);
-    mThread.start();
-  }
-
-  public void updateGameState() {
-    Log.v(TAG, "onTick");
-    // mRenderer.mAngleX += 1;
-    requestRender();
-  }
-
-  @Override public boolean onTrackballEvent(MotionEvent e) {
-    //mRenderer.mAngleX += e.getX() * TRACKBALL_SCALE_FACTOR;
-    //mRenderer.mAngleY += e.getY() * TRACKBALL_SCALE_FACTOR;
-
-    requestRender();
-    return true;
-  }
-
-  @Override public boolean onTouchEvent(MotionEvent e) {
-    float x = e.getX();
-    float y = e.getY();
-    //switch (e.getAction()) {
-    //case MotionEvent.ACTION_MOVE:
-    //  float dx = x - mPreviousX;
-    //  float dy = y - mPreviousY;
-    //  mRenderer.mAngleX += dx * TOUCH_SCALE_FACTOR;
-    //  mRenderer.mAngleY += dy * TOUCH_SCALE_FACTOR;
-    //  requestRender();
-    //}
-    //mPreviousX = x;
-    //mPreviousY = y;
-    return true;
-  }
-
-  private class PongRenderer implements GLSurfaceView.Renderer {
-    public PongRenderer() {
-      mPaddles[0] = new Paddle(Player.HUMAN, new Point(10, 210));
-      mPaddles[1] = new Paddle(Player.COMPUTER, new Point(10, 10));
-    }
-
-    public void onDrawFrame(GL10 gl) {
-      /*
-       * Usually, the first thing one might want to do is to clear
-       * the screen. The most efficient way of doing this is to use
-       * glClear().
-       */
-
-      gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-
-      /*
-       * Now we're ready to draw some 3D objects
-       */
-
-      gl.glMatrixMode(GL10.GL_MODELVIEW);
-      for (Paddle paddle : mPaddles) {
-        gl.glLoadIdentity();
-
-        // Position the paddle based on its current location.
-        Point location = paddle.getLocation();
-
-        if (paddle.getPlayer() == Player.COMPUTER) {
-          gl.glTranslatef(0.0f, 1.0f, -3.0f);
-        } else {
-          gl.glTranslatef(0.0f, -1.0f, -3.0f);
-        }
-        //gl.glTranslatef(location.x, location.y, -3.0f);
-
-        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-
-        paddle.draw(gl);
-      }
-    }
-
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-      gl.glViewport(0, 0, width, height);
-
-      /*
-       * Set our projection matrix. This doesn't have to be done
-       * each time we draw, but usually a new projection needs to
-       * be set when the viewport is resized.
-       */
-
-      float ratio = (float) width / height;
-      gl.glMatrixMode(GL10.GL_PROJECTION);
-      gl.glLoadIdentity();
-      gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
-    }
-
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-      /*
-       * By default, OpenGL enables features that improve quality
-       * but reduce performance. One might want to tweak that
-       * especially on software renderer.
-       */
-      gl.glDisable(GL10.GL_DITHER);
-
-      /*
-       * Some one-time OpenGL initialization can be made here
-       * probably based on features of this particular context
-       */
-      gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                GL10.GL_FASTEST);
-
-
-      gl.glClearColor(1,1,1,1);
-      gl.glEnable(GL10.GL_CULL_FACE);
-      gl.glShadeModel(GL10.GL_SMOOTH);
-      gl.glEnable(GL10.GL_DEPTH_TEST);
-    }
-
-    private Paddle[] mPaddles = new Paddle[2];
-  }
-
-
-  private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
-  private final float TRACKBALL_SCALE_FACTOR = 36.0f;
-
-  private PongRenderer mRenderer;
 }
